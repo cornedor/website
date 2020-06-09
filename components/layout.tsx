0c9dee7960b9f53f848aa3fa1466aedda46b1b4b
Corné Dorrestijn <contact@corne.info>
2020-06-09 23:18:43 +0200

    Added compat/responsive meta tags

--- components/layout.tsx
+++ components/layout.tsx
@@ -24,6 +24,8 @@ export default function Layout({ children, title }: LayoutProps) {
     <div className={styles.container}>
       <Head>
         <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
         <meta
           name="description"
           content="Learn how to build a personal website using Next.js"
