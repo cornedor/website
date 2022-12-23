07d6ab338f58af513e1162f08af2b3cb68ab7644
Corné Dorrestijn <cdorrestijn@emico.nl>
2022-12-23 21:59:25 +0100

    Removed cookiebot code

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -21,32 +21,6 @@ export default class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
-          <Script
-            id="Cookiebot"
-            src="https://consent.cookiebot.com/uc.js"
-            data-cbid="b772e023-b834-403e-a781-509ee5e914fb"
-            type="text/javascript"
-            async
-            strategy="afterInteractive"
-          ></Script>
-          <Script
-            id="hotjar"
-            strategy="afterInteractive"
-            data-cookieconsent="statistics"
-            type="text/plain"
-            dangerouslySetInnerHTML={{
-              __html: `
-              (function(h,o,t,j,a,r){
-                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-                h._hjSettings={hjid:3204957,hjsv:6};
-                a=o.getElementsByTagName('head')[0];
-                r=o.createElement('script');r.async=1;
-                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-                a.appendChild(r);
-              })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-              `,
-            }}
-          />
         </body>
       </Html>
     )
